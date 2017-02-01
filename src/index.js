0aee3e9c1d541384a24183894644fdcbbc2eaebc
Tobias Reich <tobias.reich.ich@gmail.com>
2017-02-01 17:32:59 +0100

    Add missing @public

--- src/index.js
+++ src/index.js
@@ -76,6 +76,7 @@ module.exports.out = function(opts) {
 /**
  * Attach an array to the function, which contains a list of
  * extensions used by the handler. The array will be used by Rosid for caching purposes.
+ * @public
  */
 module.exports.cache = [
 	'.scss',
